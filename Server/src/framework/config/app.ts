a40605dca4b60d765c84b8c652e43a8b0e589eae
4shaham <143824921+4shaham@users.noreply.github.com>
2024-10-26 12:24:55 +0530

    added

--- Server/src/framework/config/app.ts
+++ Server/src/framework/config/app.ts
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin:"https://task-management-pevubz64r-shahams-projects.vercel.app",
+    origin:"https://task-management-frontend.shaham.website",
     // origin:"http://localhost:5173",
     credentials:true
 }))
